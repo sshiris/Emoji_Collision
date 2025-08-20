84b1215815ed4a3502081a9ed7c01846a49e3b37
Iris <148431646+iris915520@users.noreply.github.com>
2025-08-20 21:06:22 +0300

    all finished

--- emoji.js
+++ emoji.js
@@ -2,19 +2,27 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const btnStart = document.getElementById("btnStart");
 
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
 let emojis = [];
 const emojiA = "🖖";
 const emojiB = "🖐️";
 const newEmoji = "🌹";
 const size = 30;
+const speed = 2;
 
 function createEmojiA() {
   const emojisA = {
     x: (Math.random() * canvas.width) / 4,
     y: Math.random() * (canvas.height / 2) + canvas.height / 4,
     emoji: emojiA,
-    dx: Math.random() - 1,
-    dy: Math.random() - 1,
+    dx: (Math.random() - 1) * speed,
+    dy: (Math.random() - 1) * speed,
   };
   return emojisA;
 }
@@ -24,14 +32,25 @@ function createEmojiB() {
     x: (Math.random() * canvas.width) / 4 + (3 * canvas.width) / 4 - size,
     y: Math.random() * (canvas.height / 2) + canvas.height / 4,
     emoji: emojiB,
-    dx: Math.random() - 1,
-    dy: Math.random() - 1,
+    dx: (Math.random() - 1) * speed,
+    dy: (Math.random() - 1) * speed,
   };
   return emojisB;
 }
 
+function createNewEmoji() {
+  const emojisNew = {
+    x: null,
+    y: null,
+    emoji: newEmoji,
+    dx: Math.random() - 1,
+    dy: Math.random() - 1,
+  };
+  return emojisNew;
+}
+
 function startGame() {
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 30; i++) {
     emojis.push(createEmojiA());
     emojis.push(createEmojiB());
   }
@@ -58,7 +77,30 @@ function updateEmojis() {
       emoji.dy *= -1;
     }
   });
+  collision();
   drawEmojis();
   requestAnimationFrame(updateEmojis);
+}
+
+function collision() {
+  for (let i = 0; i < emojis.length; i++) {
+    for (let j = i + 1; j < emojis.length; j++) {
+      let a = emojis[i];
+      let b = emojis[j];
+      if (Math.abs(a.x - b.x) < size && Math.abs(a.y - b.y) < size) {
+        if (
+          (a.emoji == emojiA && b.emoji == emojiB) ||
+          (a.emoji == emojiB && b.emoji == emojiA)
+        ) {
+          const newEmoji = createNewEmoji();
+          newEmoji.x = (a.x + b.x) / 2;
+          newEmoji.y = (a.y + b.y) / 2;
+          emojis.splice(j, 1);
+          emojis.splice(i, 1, newEmoji);
+          break;
+        }
+      }
+    }
+  }
 }
 btnStart.addEventListener("click", startGame);
